502b3a83921291a545b6e91f8106d0448b9cd999
FromTheNightJourney <andrew.tanuwijaya001@binus.ac.id>
2025-09-27 03:33:25 +0700

    Edit: changed structure, forgot you need to name folders and then put page.tsx

--- src/app/about.tsx
+++
@@ -1,9 +0,0 @@
-// app/about/page.tsx
-export default function AboutPage() {
-  return (
-    <main className="p-8">
-      <h1 className="text-2xl font-bold">About Me</h1>
-      {/* Add your details here later [cite: 21] */}
-    </main>
-  );
-}
---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-2xl font-bold">About Me SKibidi</h1>
+      {/* this is a placeholder sussy */}
+    </main>
+  );
+}
